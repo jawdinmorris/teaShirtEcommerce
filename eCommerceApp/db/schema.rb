f23f1434da5851e2dcbf6232e4e4dd2c3f71b550
Nick Pasquariello <npasquariello@gmail.com>
2018-04-04 14:01:25 +1000

    Added oder scaffold

--- eCommerceApp/db/schema.rb
+++ eCommerceApp/db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180404014339) do
+ActiveRecord::Schema.define(version: 20180404035633) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -26,6 +25,15 @@ ActiveRecord::Schema.define(version: 20180404014339) do
     t.index ["order_id"], name: "index_items_on_order_id"
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.float "total_price"
+    t.float "sub_total"
+    t.float "gst"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
