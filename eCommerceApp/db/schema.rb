f10b561e9fba9a688733a2e22114fc8b461ef774
Kito Clark <kitoclark1989@gmail.com>
2018-04-04 14:04:00 +1000

    Added bootstrap navbar to top

--- eCommerceApp/db/schema.rb
+++ eCommerceApp/db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180404014339) do
+ActiveRecord::Schema.define(version: 20180404030914) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -25,7 +24,6 @@ ActiveRecord::Schema.define(version: 20180404014339) do
     t.datetime "updated_at", null: false
     t.index ["order_id"], name: "index_items_on_order_id"
   end
-
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
