f4086b89198403eae8449c118d243f0cbae61c3e
Kito Clark <kitoclark1989@gmail.com>
2018-04-04 15:37:18 +1000

    Fixed item belong error

--- eCommerceApp/db/schema.rb
+++ eCommerceApp/db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180404030914) do
-
+ActiveRecord::Schema.define(version: 20180404043212) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -27,17 +25,8 @@ ActiveRecord::Schema.define(version: 20180404030914) do
     t.index ["order_id"], name: "index_items_on_order_id"
   end
 
-
-  create_table "orders", force: :cascade do |t|
-    t.float "total_price"
-    t.float "sub_total"
-    t.float "gst"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_orders_on_user_id"
-  end
-
+# Could not dump table "orders" because of following StandardError
+#   Unknown type 'array' for column 'order_items'
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
