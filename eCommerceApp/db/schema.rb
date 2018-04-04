533c4d6f8d0e9f119e8b5f60f61b75e2c5b1ae0e
Kito Clark <kitoclark1989@gmail.com>
2018-04-04 20:37:55 +1000

    Styled navbar and items page. Added update funcionality to items page

--- eCommerceApp/db/schema.rb
+++ eCommerceApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404052215) do
+ActiveRecord::Schema.define(version: 20180404085024) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "item_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20180404052215) do
     t.integer "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["order_id"], name: "index_items_on_order_id"
   end
 
