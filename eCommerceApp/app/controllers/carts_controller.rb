534a398f7efaf97e0fa85a33cb636a7d41c4cd72
Kito Clark <kitoclark1989@gmail.com>
2018-04-05 15:16:46 +1000

    Fixed merge issues

--- eCommerceApp/app/controllers/carts_controller.rb
+++ eCommerceApp/app/controllers/carts_controller.rb
@@ -31,7 +31,7 @@ class CartsController < ApplicationController
 
     respond_to do |format|
       if @cart.save
-        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
+        format.html { redirect_to carts_path, notice: 'Cart was successfully created.' }
         format.json { render :show, status: :created, location: @cart }
       else
         format.html { render :new }
@@ -45,7 +45,7 @@ class CartsController < ApplicationController
   def update
     respond_to do |format|
       if @cart.update(cart_params)
-        format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
+        format.html { redirect_to carts_path, notice: 'Cart was successfully updated.' }
         format.json { render :show, status: :ok, location: @cart }
       else
         format.html { render :edit }
