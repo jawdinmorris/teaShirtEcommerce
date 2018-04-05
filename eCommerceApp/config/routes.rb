27a9002f16b025cddd7a62d7721e6a1470ecfc96
[jawdinmorris] <[jordanandrewmorris1@gmail.com]>
2018-04-05 18:44:36 +1000

    Fixed user errors, fixed item/cart errors

--- eCommerceApp/config/routes.rb
+++ eCommerceApp/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :carts
   resources :orders
   resources :items
-    resources :users
+
   root to: "pages#index"
   devise_for :users, controllers: {
     sessions: 'users/sessions'
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   get '/admin_show', to: 'items#admin_show'
   get '/index', to: 'users#index'
 
-
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
